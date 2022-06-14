bca6680910a6d1ace50276ee4e8b4e855e8a499d
Lyn <lynfinman@gmail.com>
2022-06-14 13:17:10 -0400

    mapped toy collection to toy cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -7,13 +7,24 @@ import ToyContainer from "./ToyContainer";
 //  └ ├ ┌ ─
 //
 //  App
-//   ├ Header
+//   ├ Header [showForm][toys]
 //   ├ ToyForm
 //   ├ .buttonContainer
 //   └ ToyContainer
 //         └ ToyCard
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys", {
+      headers: {
+        "Content-Type": "application-json",
+      },
+    })
+      .then((r) => r.json())
+      .then((toys) => setToys(toys));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -26,7 +37,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
